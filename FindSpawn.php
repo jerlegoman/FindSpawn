0263ff94b468f799c4dfd9d9107aa4f6092d4981
jerlegoman <jeremyhaft@yahoo.com>
2014-06-16 14:29:31 -0400

    Create FindSpawn.php
    My fist plugin!

---
+++ FindSpawn.php
@@ -0,0 +1,30 @@
+<?php
+/*
+__PocketMine Plugin__
+name=TOspawn
+version=1.0
+apiversion=12
+author=Jerlegoman
+class=spaplus
+*/
+class spaplus implements Plugin {
+private $api;
+public function __construct(ServerAPI $api,$server = false) {
+$this->api = $api;
+}
+public function init() {
+$this->api->addHandler("player.action", array($this, "eventHandle"), 50);
+}
+public function eventHandle($data, $event) {
+switch ($event) {
+case "player.action":
+$player = $data["player"];
+$item = $player->getSlot($player->slot);
+if($item->getid()==345){
+$data->sendChat("** Have Fun and BE CARE FULL!");
+}
+}
+}
+public function __destruct(){}
+}
+?>
